58da2f081d95efe0b210202b39ba91fb8df88165
Ajongafac Zinkeng <ajongafaczinkeng@gmail.com>
2025-07-28 09:10:16 +0100

    update

--- az-portfolio/src/components/heroSection/HeroPic.jsx
+++ az-portfolio/src/components/heroSection/HeroPic.jsx
@@ -13,7 +13,7 @@ const HeroPic = () => {
       className="h-full flex items-center justify-center"
     >
       <img
-        src="../../public/images/HexaPic.png"
+        src="../../public/images/AJ2.png"
         alt="Ajongafac Zinkeng"
         className="max-h-[450px] w-auto "
       />
